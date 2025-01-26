80fdbc7cd1a284be36388b70f5cbe2e1f931cb45
Callum Brezden <callumbrezden@gmail.com>
2025-01-25 19:17:20 -0800

    Steam Connection Working

---
+++ README.md
@@ -0,0 +1,10 @@
+# Godot Peer-2-Peer Networking using Steamworks SDK
+
+This is supposed to be a simple example of how to use the Steamworks 
+SDK to create a peer-2-peer connection between two Godot games.
+
+## How to use
+1. Download the Steamworks SDK from [here](https://partner.steamgames.com/)
+2. Extract the `steam_api.dll` and `steam_api64.dll` files to the root of the project
+3. Download the Facepunch Steamworks module from the releases page [here](https://github.com/Facepunch/Facepunch.Steamworks/releases)
+4. Extract the `Facepunch.Steamworks.Win64.dll` into the `DLL` folder.
--- Steam/SteamManager.cs
+++ Steam/SteamManager.cs
@@ -1,13 +1,39 @@
 using Godot;
 using System;
+using Steamworks;
 
 public partial class SteamManager : Node
 {
-	public override void _Ready()
-	{
-	}
+    public override void _Ready()
+    {
+        try
+        {
+            GD.Print("Attempting to initialize Steam...");
+            SteamClient.Init(3485870, true);
 
-	public override void _Process(double delta)
-	{
-	}
+            if (SteamClient.IsValid)
+            {
+                GD.Print("Steam initialized successfully!");
+                GD.Print($"Steam Username: {SteamClient.Name}");
+            }
+            else
+            {
+                GD.PrintErr("Steam initialization failed.");
+            }
+        }
+        catch (Exception ex)
+        {
+            GD.PrintErr($"Steam initialization error: {ex.Message}");
+        }
+    }
+
+    public override void _Process(double delta)
+    {
+        SteamClient.RunCallbacks();
+    }
+
+    public override void _ExitTree()
+    {
+        SteamClient.Shutdown();
+    }
 }
