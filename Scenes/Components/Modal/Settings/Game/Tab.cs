97ebcaa7f9c09bf2f5116199615489d9311fceea
Callum Brezden <callumbrezden@gmail.com>
2025-08-09 19:35:10 -0700

    16: Tab Setup

---
+++ Scenes/Components/Modal/Settings/Game/Tab.cs
@@ -0,0 +1,11 @@
+using Godot;
+
+namespace GodotPeer2PeerSteamCSharp.Scenes.Components.Modal.Settings.Game;
+
+public partial class Tab: Control
+{
+    public override void _Ready()
+    {
+        
+    }
+}
