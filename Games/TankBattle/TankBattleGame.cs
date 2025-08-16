82fa0f1a5d0fc7cf1bd46ddb2a4736ba5540dd80
Callum Brezden <callumbrezden@gmail.com>
2025-08-16 00:59:52 -0700

    0: Game files reorganized and container created

--- Games/TankBattle/TankBattleGame.cs
+++
@@ -1,8 +0,0 @@
-using GodotPeer2PeerSteamCSharp.Games;
-using GodotPeer2PeerSteamCSharp.Types.Games;
-
-public class TankBattleGame : IGame
-{
-    public string GameName => "Tank Battle";
-    public GameType GameType => GameType.FreeForAll;
-}
