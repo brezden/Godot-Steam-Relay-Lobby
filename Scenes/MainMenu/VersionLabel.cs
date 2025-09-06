8c0bde44e4deddff0d1999c327741562cf3204fe
Callum Brezden <callumbrezden@gmail.com>
2025-09-06 12:53:31 -0400

    v0.1.1-alpha.1 (#20) - Semantic Versioning Added

---
+++ Scenes/MainMenu/VersionLabel.cs
@@ -0,0 +1,18 @@
+using Godot;
+
+public partial class VersionLabel : Label
+{
+    public override void _Ready()
+    {
+        string v = "0.0.0";
+        const string path = "res://VERSION";
+
+        if (FileAccess.FileExists(path))
+        {
+            using var f = FileAccess.Open(path, FileAccess.ModeFlags.Read);
+            v = f.GetAsText().Trim();   // trims \n/\r\n
+        }
+
+        Text = $"v{v}";
+    }
+}
